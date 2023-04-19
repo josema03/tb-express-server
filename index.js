56af7a69afa1b3c81b4e62fdf4a23bc90ab46663
Jose Marin <jose.marin1997@gmail.com>
2023-04-19 13:29:29 -0400

    feat: cors

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const { PORT } = require('./src/constants')
-const filesRouter = require('./src/routes/files')
+const express = require('express');
+const { PORT } = require('./src/constants');
+const filesRouter = require('./src/routes/files');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use('/files', filesRouter)
+app.use(cors());
+
+app.use('/files', filesRouter);
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-})
+    console.log(`Listening on port ${PORT}`);
+});
 
-module.exports = app
+module.exports = app;
